c5416399366ac1ecdf80ebf5602fd2c32a86cba8
Niko Köbler <niko@n-k.de>
2024-03-14 14:10:30 +0100

    part 6

--- resource/src/main/java/dasniko/resourceserver/JwtService.java
+++ resource/src/main/java/dasniko/resourceserver/JwtService.java
@@ -1,7 +1,53 @@
 package dasniko.resourceserver;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.smallrye.jwt.auth.principal.JWTParser;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.security.auth.login.CredentialExpiredException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class JwtService {
+
+	@Inject
+	ObjectMapper objectMapper;
+	@Inject
+	ApplicationState app;
+	@Inject
+	JWTParser jwtParser;
+
+	JsonWebToken verify(String tokenString) throws Exception {
+		// find proper key
+		String[] parts = tokenString.split("\\.");
+		String headerString = new String(Base64.getDecoder().decode(parts[0]));
+		Map<String, String> header = objectMapper.readValue(headerString, new TypeReference<>() {});
+		String kid = header.get("kid");
+
+		List<Map<String, Object>> keys = (List<Map<String, Object>>) app.getJsonWebKeySet().get("keys");
+		Map<String, Object> key = keys.stream().filter(k -> k.get("kid").equals(kid)).findFirst().orElseThrow();
+
+		// create public key for verification
+		BigInteger n = new BigInteger(1, Base64.getUrlDecoder().decode((String) key.get("n")));
+		BigInteger e = new BigInteger(1, Base64.getUrlDecoder().decode((String) key.get("e")));
+		RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(n, e);
+		PublicKey publicKey = KeyFactory.getInstance((String) key.get("kty")).generatePublic(rsaPublicKeySpec);
+
+		// return token if valid
+		JsonWebToken token = jwtParser.verify(tokenString, publicKey);
+		if (token.getExpirationTime() < (System.currentTimeMillis() / 1000)) {
+			throw new CredentialExpiredException("token is expired");
+		}
+		return token;
+	}
+
 }
--- resource/src/main/java/dasniko/resourceserver/SecurityContainerRequestFilter.java
+++ resource/src/main/java/dasniko/resourceserver/SecurityContainerRequestFilter.java
@@ -1,17 +1,45 @@
 package dasniko.resourceserver;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import java.util.Map;
 
 @Slf4j
 @Provider
 public class SecurityContainerRequestFilter implements ContainerRequestFilter {
 
+	@Inject
+	JwtService jwtService;
+
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
-		// TODO implement me!
+		Response.ResponseBuilder response = null;
+		String authorization = requestContext.getHeaderString("X-Authorization"); // don't do this at home!!!
+
+		if (authorization == null || !"bearer ".equalsIgnoreCase(authorization.substring(0, 7))) {
+			response = Response.status(Response.Status.UNAUTHORIZED);
+		} else {
+
+			try {
+				String tokenString = authorization.substring(7);
+				JsonWebToken token = jwtService.verify(tokenString);
+				// check token on further claims
+			} catch (Exception e) {
+				response = Response.status(Response.Status.UNAUTHORIZED).entity(Map.of("error", e.getMessage()));
+			}
+
+		}
+
+		if (response != null) {
+			requestContext.abortWith(response.type(MediaType.APPLICATION_JSON).build());
+		}
 	}
 
 }
