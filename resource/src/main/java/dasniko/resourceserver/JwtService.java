1a655e2a15da66f0b87a52c2e406bf7ef5e51dd0
Niko Köbler <niko@n-k.de>
2025-05-09 14:33:31 +0200

    resource server

--- resource/src/main/java/dasniko/resourceserver/JwtService.java
+++ resource/src/main/java/dasniko/resourceserver/JwtService.java
@@ -1,7 +1,46 @@
 package dasniko.resourceserver;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.smallrye.jwt.auth.principal.JWTParser;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+import java.util.Map;
 
 @ApplicationScoped
 public class JwtService {
+
+	@Inject
+	ObjectMapper objectMapper;
+	@Inject
+	ApplicationState app;
+	@Inject
+	JWTParser jwtParser;
+
+	JsonWebToken verify(String tokenString) throws Exception {
+		// find proper key
+		String[] parts = tokenString.split("\\.");
+		String headerString = new String(Base64.getDecoder().decode(parts[0]));
+		Map<String, String> header = objectMapper.readValue(headerString, new TypeReference<>() {});
+		String kid = header.get("kid");
+
+		Map<String, Object> key = app.getJsonWebKey(kid);
+
+		// create public key for verification
+		BigInteger modulus = new BigInteger(1, Base64.getUrlDecoder().decode((String) key.get("n")));
+		BigInteger exponent = new BigInteger(1, Base64.getUrlDecoder().decode((String) key.get("e")));
+		RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(modulus, exponent);
+		PublicKey publicKey = KeyFactory.getInstance("RSA")
+			.generatePublic(publicKeySpec);
+
+		// return token if valid
+		return jwtParser.verify(tokenString, publicKey);
+	}
 }
--- resource/src/main/java/dasniko/resourceserver/SecurityContainerRequestFilter.java
+++ resource/src/main/java/dasniko/resourceserver/SecurityContainerRequestFilter.java
@@ -1,17 +1,43 @@
 package dasniko.resourceserver;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import java.util.Map;
 
 @Slf4j
 @Provider
 public class SecurityContainerRequestFilter implements ContainerRequestFilter {
 
+	@Inject
+	JwtService jwtService;
+
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
-		// TODO implement me!
+		Response.ResponseBuilder response = null;
+
+		String authorization = requestContext.getHeaderString("X-Authorization");
+		if (authorization == null || !"bearer ".equalsIgnoreCase(authorization.substring(0, 7))) {
+			response = Response.status(Response.Status.UNAUTHORIZED);
+		} else {
+
+			try {
+				String tokenString = authorization.substring(7);
+				JsonWebToken token = jwtService.verify(tokenString);
+			} catch (Exception e) {
+				response = Response.status(Response.Status.UNAUTHORIZED)
+					.entity(Map.of("error", e.getMessage()));
+			}
+		}
+
+		if (response != null) {
+			requestContext.abortWith(response.type("application/json").build());
+		}
 	}
 
 }
